10bfa1919dce996255a9304d22e61ac91f995da8
thechutrain <chu.chuyalan@gmail.com>
2019-04-07 19:26:00 -0400

    needed to clear the line[] array!

--- day_1/day_1_solution.c
+++ day_1/day_1_solution.c
@@ -6,17 +6,43 @@
  */
 
 #include <stdio.h>
-#define max_int_size 5
+#include <string.h>
+
+// #define max_int_size 5
 // #define file_name frequency.txt // DOESNT WORK
 char file_name[] = "test.txt";
 
 //int isPositive(){}
 
+void clearBuffer(char *line, int size) {
+  int i;
+  for (i=0; i<size; i++) {
+    line[i]='\0'; 
+  }
+  puts(line);
+}
+
 void readEachLine(FILE *fp) {
-  char line[max_int_size + 1]; // since its also going to take +/-
+  char line[10]; 
   char ch;
+  int i = 0;
+
   while((ch=fgetc(fp)) != EOF) {
-    printf("%c\n", ch);
+    //printf("%c\n", ch); 
+    if (ch == '\n') {
+      // FOR DEBUGGING:
+      puts(line);
+      printf("String length: %lu\n", strlen(line));
+    
+      // reset:  
+      i = 0;
+      clearBuffer(line, 10);
+     
+      // call the addition function
+    } else {
+      line[i] = ch;
+      i++;
+    }
   }
 }
 
